ab0d005b88cea693424a69d6b3d7991fa7e927ba
Imanol Enriquë <76859196+Husdady@users.noreply.github.com>
2021-01-17 14:51:43 -0500

    Add files via upload

---
+++ components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/footer.css';
+
+class Footer extends Component {
+
+render(){
+  return(
+    <footer>Creado por <a href="https://www.linkedin.com/in/imanol-enriqu%C3%AB">Husdady</a> &copy;</footer>
+  )
+}
+
+}
+
+export default Footer;
